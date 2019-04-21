cf85cef0116e18dafeebe8380ada122df489cd48
Anurag Patil <41022671+Anurag-104@users.noreply.github.com>
2019-04-21 17:55:28 +0530

    Update tree_generator.py

--- tree_generator.py
+++ tree_generator.py
@@ -1,4 +1,5 @@
-from anytree import Node
+from anytree import Node, RenderTree
+from anytree.exporter import DotExporter
 import string
 
 precedence = {
@@ -15,37 +16,59 @@ ops = list(precedence.keys())
 
 def infix_to_postfix(elements):
     opstack = []
-    output = []
+    outputer = []
     for element in elements:
-        print("\nOpstack: ", opstack)
-        print("Output: ", output)
-        print("Element added:", element)
         if element in letters:
-            output.append(element)
+            outputer.append(element)
             continue
         if element in ops:
             if len(opstack) == 0:
                 opstack.append(element)
                 continue
-            if precedence[element] >= precedence[opstack[-1]]:
+            if precedence[element] > precedence[opstack[-1]]:
                 opstack.append(element)
                 continue
-            if precedence[element] < precedence[opstack[-1]]:
+            if precedence[element] <= precedence[opstack[-1]]:
                 while True:
-                    output.append(opstack.pop())
+                    outputer.append(opstack.pop())
                     if len(opstack) == 0:
                         break
-                    if not precedence[element] < precedence[opstack[-1]]:
+                    if not precedence[element] <= precedence[opstack[-1]]:
                         break
                 opstack.append(element)
 
     while len(opstack) > 0:
-        output.append(opstack.pop())
+        outputer.append(opstack.pop())
 
-    return output
+    return outputer
 
 
 output = infix_to_postfix("d=z*a/b+c")
 
-print("\n----- Done -----")
-print(output)
+print("Postfix Form of the equation : ", output)
+
+nodedict = {}
+
+listed = []
+
+for i in output:
+    if i not in ops:
+        listed.append(Node(i))
+        # print(listed)
+    elif i in ops:
+        nodedict[i] = Node(i)
+        # print(nodedict)
+        pop1, pop2 = listed.pop(), listed.pop()
+        nodedict[i].children = [pop2, pop1]
+        listed.append(Node(i))
+print(nodedict['+'].children)
+print("Nodes : ", nodedict)
+node = []
+
+# for i in range(len(nodedict.keys())):
+#     node.append(list(nodedict.keys())[i])
+#     node[i].children = list(nodedict.values())[i]
+#     print(node[i].children)
+
+for pre, _, noder in RenderTree(nodedict['=']):
+    print("%s%s" % (pre, noder))
